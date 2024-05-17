e9cd79d6739fe3f5f3c889c799c10662e99a702f
ronau <5237327+ronau@users.noreply.github.com>
2024-05-17 08:33:22 +0200

    Add first simple math method

---
+++ math_demo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# This script will showcase how to do simple math operations in Python
+
+
+# Gauss summation
+def summation(n):
+    return int(n*(n+1)/2)
+
+number = 10
+print(f"The Gauss summation of {number} is {summation(number)}")
